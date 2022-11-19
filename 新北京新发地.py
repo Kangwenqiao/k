05e65016f3379a7aa11b6aa79cc668b7b1713730
Kangwenqiao <118408678+Kangwenqiao@users.noreply.github.com>
2022-11-19 20:15:27 +0800

    Update 新北京新发地.py

--- 新北京新发地.py
+++ 新北京新发地.py
@@ -2,8 +2,8 @@ import requests
 import csv
 import re
 url = "http://www.xinfadi.com.cn/getPriceData.html"
-a=int(input("start page:"))
-b=int(input("end page:"))
+a=int(input("enter the web start page:"))
+b=int(input("enter the web end page:"))
 for i in range(a,b+1,1):
     data={
      "limit":"",
@@ -18,14 +18,14 @@ for i in range(a,b+1,1):
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit"
                       "/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 S"
                       "afari/537.36"
-    }
+    }#伪装成chrom浏览器
 
     resp=requests.post(url=url,headers=header,data=data)
     page=resp.text
-    # print(page)
-    obj=re.compile(r'"id":(?P<id>.*?),"prodName":"(?P<prodName>.*?)","prodCatid":(?P<prodCatid>.*?),"prodCat":"(?P<prodCat>.*?)",'
-                   r'"prodPcatid":(?P<prodPcatid>.*?),"prodPcat":"(?P<prodPcat>.*?)","lowPrice":"(?P<lowPrice>.*?)","highPrice":"(?P<highPrice>.*?)",'
-                   r'"avgPrice":"(?P<avgPrice>.*?)","place":"(?P<place>.*?)","specInfo":"(?P<specInfo>.*?)","unitInfo":"(?P<unitInfo>.*?)",'
+    print(page)
+    obj=re.compile(r'"id":(?P<id>.*?),"prodName":"(?P<prodName>.*?)","prodCatid":.*?,"prodCat":"(?P<prodCat>.*?)",'
+                   r'"prodPcatid":.*?,"lowPrice":"(?P<lowPrice>.*?)","highPrice":"(?P<highPrice>.*?)",'
+                   r'"avgPrice":"(?P<avgPrice>.*?)",.*?,"specInfo":"(?P<specInfo>.*?)","unitInfo":"(?P<unitInfo>.*?)",'
                    r'.*?}',re.S)
 
     result=obj.finditer(page)
