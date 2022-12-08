b13046bfbc3158a6baf4c54d4226381b9879b8b5
heguoyuan <118408678+Kangwenqiao@users.noreply.github.com>
2022-12-08 17:53:15 +0800

    Update 哈希实验报告.cpp

--- 哈希实验报告.cpp
+++ 哈希实验报告.cpp
@@ -18,19 +18,19 @@ int p;
 status create(list_ l)
 {
 	int leng; int i, j,num;
-	cin >> leng;
+	cin >> leng;//输入元素个数
 	l->length = leng;
 	for (i = 1; i <= l->length; i++)
 	{
 		cin >> num;
 		l[i].num = num;
-		l[i].state = uncreate;
+		l[i].state = uncreate;//加入初始数组的状态全都是未载入哈希
 	}
 	
 	cin >> p;
 	return succ;
 }
-int hs_function(int n)
+int hs_function(int n)//哈希函数
 {
 	int hs;
 	hs = n % p;
@@ -43,22 +43,22 @@ status hs(list_ l, key k)
 
 	for (i = 0; i <= p; i++)
 	{
-		k[i].vexs = maxint;
+		k[i].vexs = maxint;//哈希列先全部取最大树为空
 	}
 
-	for (i = 1; i <= l->length; i++)
+	for (i = 1; i <= l->length; i++)//将数字写入散列
 	{
-		key_ = hs_function(l[i].num);
+		key_ = hs_function(l[i].num);//哈希函数求key值
 		l[i].key = key_;
 		if (k[key_].vexs==maxint)
-			k[key_].vexs = l[i].num, l[i].state = succ;
+			k[key_].vexs = l[i].num, l[i].state = succ;//对成功写入的进行标记成功
 	}
 	return succ;
 }
-status dea_conflict(list_ l, key k)
+status dea_conflict(list_ l, key k)//冲突处理
 {
 	int i, j;
-	for (i = 1; i <= l->length; i++)
+	for (i = 1; i <= l->length; i++)//先往下寻找空位
 	{
 		if (l[i].state == uncreate)
 		{
@@ -68,7 +68,7 @@ status dea_conflict(list_ l, key k)
 			}
 		}
 	}
-	for (i = 1; i <= l->length; i++)
+	for (i = 1; i <= l->length; i++)//然后从表头开始
 	{
 		if (l[i].state == uncreate)
 		{
@@ -79,10 +79,10 @@ status dea_conflict(list_ l, key k)
 		}
 	}
 
-	for (i = 0; i < p; i++)
+	for (i = 0; i < p; i++)//打印散列
 	{
 		if(k[i].vexs!=maxint)
-		cout << k[i].vexs<<' ';
+		cout << k[i].vexs<<' '<<i<<endl;
 	}
 
 	return succ;
